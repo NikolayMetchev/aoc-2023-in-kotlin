8ca7d43e038f5ee634f0383170c117566aed6369
Nikolay Metchev <nmetchev@paxos.com>
2023-12-13 13:24:44 +0000

    solved day 12

--- src/Day12.kt
+++ src/Day12.kt
@@ -19,7 +19,6 @@ data class SpringData(val specs: String, val springLengths: List<Int>) {
 val invalidEnds = listOf('?', '#')
 
 fun main() {
-
     fun parse(input: List<String>): List<SpringData> {
         return input.map {
             val (left, right) = it.split(" ")
@@ -37,14 +36,34 @@ fun main() {
         }
         return bitSet
     }
+    data class Key(val spec: String, val lengths: IntArray) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Key
+
+            if (spec != other.spec) return false
+            if (!lengths.contentEquals(other.lengths)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = spec.hashCode()
+            result = 31 * result + lengths.contentHashCode()
+            return result
+        }
+    }
 
     fun computeArrangements(
         spec: String,
         startingIndex: Int,
-        startLengthsIndex: Int,
-        springLengths: List<Int>,
-    ) : Int {
-        if (startLengthsIndex == springLengths.size) {
+        springLengthsIndex: Int,
+        springLengths: IntArray,
+        cache: MutableMap<Key, Long>,
+    ) : Long {
+        if (springLengthsIndex == springLengths.size) {
             val newSpec = spec.replace("?", ".")
 //            println("adding spec = $newSpec")
             if (!newSpec.contains('#')) {
@@ -55,14 +74,22 @@ fun main() {
         if (startingIndex >= spec.length) {
             return 0
         }
+        if (startingIndex >= spec.length) {
+            return 0
+        }
         if ((0 ..< startingIndex).any { spec[it] == '#' }) {
             return 0
         }
-        val firstSize = springLengths[startLengthsIndex]
+        val key = Key(spec.substring(startingIndex), springLengths.copyOfRange(springLengthsIndex, springLengths.lastIndex))
+        val cached = cache[key]
+        if (cached != null) {
+            return cached
+        }
+        val firstSize = springLengths[springLengthsIndex]
 //        val rest = springLengths.drop(1)
 //        val restSum = springLengths.sumOf {  }
-        val restRange: IntRange = startLengthsIndex + 1.. springLengths.lastIndex
-        val restSum = restRange.sumOf { springLengths[it] } + (springLengths.lastIndex - startLengthsIndex)
+        val restRange: IntRange = springLengthsIndex + 1.. springLengths.lastIndex
+        val restSum = restRange.sumOf { springLengths[it] } + (springLengths.lastIndex - springLengthsIndex)
         val substring2 = spec.substring(startingIndex, spec.length)
 //        println("Substring to check $substring2")
         val newSpecs = mutableListOf<Pair<String, Int>>()
@@ -70,7 +97,7 @@ fun main() {
         val lastValidIndex = (spec.length - restSum).coerceAtMost(spec.length - firstSize)
         val validRange = startingIndex..lastValidIndex
         val firstHashIndex = validRange.firstOrNull() { spec[it] == '#' } ?: Int.MAX_VALUE
-        val minFirstIndex = if (startLengthsIndex == springLengths.lastIndex) {
+        val minFirstIndex = if (springLengthsIndex == springLengths.lastIndex) {
             spec.lastIndexOf('#') - firstSize + 1
         } else {
             0
@@ -130,35 +157,37 @@ fun main() {
                 }
             }
 //        println("spec = $spec, size=$firstSize, rest = $rest, newSpecs = $newSpecs")
-        return newSpecs.sumOf {
+        val sumOf = newSpecs.sumOf {
             if (it.first.length != spec.length) {
                 error("newSpecs.size != spec.length")
             }
 //            println("${it} rest=$rest")
-            computeArrangements(it.first, it.second, startLengthsIndex + 1, springLengths)
+            computeArrangements(it.first, it.second, springLengthsIndex + 1, springLengths, cache)
         }
+        cache[key] = sumOf
+        return sumOf
     }
 
     fun computeAnswer(springSpec: List<SpringData>) = springSpec.sumOf {
-        val ans  = computeArrangements(it.specs, 0, 0, it.springLengths)
+        val ans  = computeArrangements(it.specs, 0, 0, it.springLengths.toIntArray(), HashMap())
 //        println("${it.specs} foundSpecs = $foundSpecs, size=${it.springLengths}: $ans")
         println("${it.specs} size=${it.springLengths}: $ans")
         ans
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val springSpec = parse(input)
 //        println(springSpec)
         return computeAnswer(springSpec)
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val springSpec = parse(input).map { it.expand() }
 //        println(springSpec)
         return computeAnswer(springSpec)
     }
 
-    fun checkPart1(input: String, expected: Int) {
+    fun checkPart1(input: String, expected: Long) {
         val part1 = part1(readInput(input))
         val part1Expected = expected
         check(part1 == part1Expected) {
@@ -166,7 +195,7 @@ fun main() {
         }
     }
 
-    fun checkPart2(input: String, expected: Int) {
+    fun checkPart2(input: String, expected: Long) {
         val part2 = part2(readInput(input))
         val part2Expected = expected
         check(part2 == part2Expected) {
@@ -175,14 +204,14 @@ fun main() {
     }
 
 //    checkPart1("Day12_test0", 1L)
-    checkPart1("Day12_test1", 4)
-    checkPart1("Day12_test2", 1)
-    checkPart1("Day12_test3", 1)
-    checkPart1("Day12_test4", 4)
-    checkPart1("Day12_test5", 10)
-    checkPart1("Day12_test", 21)
-    checkPart1("Day12", 7732)
-    checkPart2("Day12_test", 525152)
+    checkPart1("Day12_test1", 4L)
+    checkPart1("Day12_test2", 1L)
+    checkPart1("Day12_test3", 1L)
+    checkPart1("Day12_test4", 4L)
+    checkPart1("Day12_test5", 10L)
+    checkPart1("Day12_test", 21L)
+    checkPart1("Day12", 7732L)
+    checkPart2("Day12_test", 525152L)
 //    checkPart2("Day12_test", 6)
 
     val input = readInput("Day12")
